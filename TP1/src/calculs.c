288bddf34a8607d19b45ad8dd5637a74ebd5ca5b
perretducrayaymeric-ctrl <perretducrayaymeric@gmail.com>
2025-12-01 13:42:31 +0100

    Implement basic calculator operations in calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,68 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+';   // Change cette valeur pour tester
+
+    int resultat;
+
+    switch (op) {
+
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Erreur : division par zero !\n");
+            } else {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                printf("Erreur : modulo par zero !\n");
+            } else {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Operateur '%c' invalide.\n", op);
+            break;
+    }
+
+    return 0;
+}
+
 
