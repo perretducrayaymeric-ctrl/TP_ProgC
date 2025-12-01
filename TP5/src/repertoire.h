7de93ba8049175b5674d247064fc7abba81e9831
perretducrayaymeric-ctrl <perretducrayaymeric@gmail.com>
2025-12-01 14:40:25 +0100

    Update repertoire.h

--- TP5/src/repertoire.h
+++ TP5/src/repertoire.h
@@ -1,9 +1,8 @@
 #ifndef REPERTOIRE_H
 #define REPERTOIRE_H
 
-// Déclaration de la fonction lire_dossier
 void lire_dossier(const char *nom_repertoire);
+void lire_dossier_recursif(const char *nom_repertoire);
+void lire_dossier_iteratif(const char *nom_repertoire);
 
 #endif // REPERTOIRE_H
-
-
