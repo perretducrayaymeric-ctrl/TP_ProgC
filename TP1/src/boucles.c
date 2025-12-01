e060c1f6831e0f9ea89b44af0fc3ab24f456eb7a
perretducrayaymeric-ctrl <perretducrayaymeric@gmail.com>
2025-12-01 13:41:10 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5;  // à modifier pour tester (doit rester < 10)
+    int compteur = 5;  // Change la valeur pour tester (DOIT être < 10)
 
-    if (compteur >= 10) {
-        printf("Erreur : compteur doit être < 10.\n");
+    if (compteur >= 10 || compteur < 1) {
+        printf("Erreur : compteur doit etre entre 1 et 9.\n");
         return 1;
     }
 
     for (int i = 1; i <= compteur; i++) {
+
         for (int j = 1; j <= i; j++) {
+
+            // À partir de la 3e ligne -> l'intérieur devient '#'
             if (i >= 3 && j > 1 && j < i) {
                 printf("# ");
             } else {
                 printf("* ");
             }
         }
+
         printf("\n");
     }
 
