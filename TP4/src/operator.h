c938de47f097f4a70e78bae45ddf960a7dbb4382
perretducrayaymeric-ctrl <perretducrayaymeric@gmail.com>
2025-12-01 14:19:32 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,14 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+// Prototypes des fonctions
+int somme(int num1, int num2);
+int difference(int num1, int num2);
+int produit(int num1, int num2);
+int quotient(int num1, int num2);
+int modulo(int num1, int num2);
+int et_bitwise(int num1, int num2);
+int ou_bitwise(int num1, int num2);
+int negation(int num1);
+
+#endif
